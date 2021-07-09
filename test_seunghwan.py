91d85a89581644e364d604b0ac63a22f54a5a7e7
P.EA <sh040119@gmail.com>
2021-07-09 14:49:59 +0900

    v_2

--- test_seunghwan.py
+++ test_seunghwan.py
@@ -1,4 +1,4 @@
-from ppadb.client import Client as AdbClient
-client = AdbClient(host="127.0.0.1", port=5037)
-devices = client.devices()
-device = devices[0]
+from  ppadb.client  import  Client  as  AdbClient 
+# 기본값은 "127.0.0.1"이고 5037 
+client  =  AdbClient ( host = "127.0.0.1" ,  port = 5037 ) 
+print ( client . version ()) 
